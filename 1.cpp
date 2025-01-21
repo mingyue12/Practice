ab6a8a38c5a556fe504f96ec6cfab7fa0aecba5d
user.name <2542407204@qq.com>
2025-01-21 17:33:39 +0800

    modified:   1.cpp 	new file:   1.exe

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,24 @@
 #include<stdio.h>
+
+int max(int a, int b)
+{
+    if(a>b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
 int main()
 {
+    int maxMum = 0;
+    int a = 0;
+    int b = 0;
+    a = 1, b = 0;
+    maxMum = max(a,b);
+    printf("%d",maxMum);
     return 0;
 }
